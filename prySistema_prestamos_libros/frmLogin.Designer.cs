039e3d248bdab3979d4ff049f7f0ce2f4d957c78
ligoh <ligoh@DELL>
2026-06-27 10:08:04 -0600

    Agregar archivos de proyecto.

---
+++ prySistema_prestamos_libros/frmLogin.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace prySistema_prestamos_libros
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnAcceder = new Button();
+            btnSalir = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(310, 318);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(330, 38);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(310, 416);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(330, 38);
+            textBox2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.logol;
+            pictureBox1.Location = new Point(344, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(259, 252);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(419, 284);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 31);
+            label1.TabIndex = 3;
+            label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(419, 382);
+            label2.Name = "label2";
+            label2.Size = new Size(129, 31);
+            label2.TabIndex = 4;
+            label2.Text = "Contraseña";
+            // 
+            // btnAcceder
+            // 
+            btnAcceder.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAcceder.Location = new Point(259, 498);
+            btnAcceder.Name = "btnAcceder";
+            btnAcceder.Size = new Size(150, 45);
+            btnAcceder.TabIndex = 5;
+            btnAcceder.Text = "Acceder";
+            btnAcceder.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSalir.Location = new Point(528, 498);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(150, 45);
+            btnSalir.TabIndex = 6;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(250, 247, 242);
+            ClientSize = new Size(976, 618);
+            Controls.Add(btnSalir);
+            Controls.Add(btnAcceder);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "frmLogin";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Button btnAcceder;
+        private Button btnSalir;
+    }
+}
---
+++ prySistema_prestamos_libros/frmLogin.cs
@@ -0,0 +1,10 @@
+namespace prySistema_prestamos_libros
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
