85e769eee71845281f74f5d1e76ec0b7af9ead32
Jesus Alexis <20251037@uthh.edu.mx.>
2026-06-28 05:12:15 -0600

    Se agregaron 4 apartados 2 Formularios y 2 Gestiones.

---
+++ prySistema_prestamos_libros/frmGestionUsuarios.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace prySistema_prestamos_libros
+{
+    partial class frmGestionUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.contenerdorGrupo;
+            pictureBox1.Location = new Point(33, 28);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1367, 797);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F);
+            label1.Location = new Point(78, 83);
+            label1.Name = "label1";
+            label1.Size = new Size(239, 23);
+            label1.TabIndex = 1;
+            label1.Text = "Buscar por numero de control";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(78, 109);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(277, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(201, 168, 76);
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            button1.ForeColor = Color.FromArgb(17, 30, 71);
+            button1.Location = new Point(712, 91);
+            button1.Name = "button1";
+            button1.Size = new Size(149, 45);
+            button1.TabIndex = 3;
+            button1.Text = "Nuevo registro";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(107, 122, 153);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(966, 91);
+            button2.Name = "button2";
+            button2.Size = new Size(149, 45);
+            button2.TabIndex = 4;
+            button2.Text = "Editar";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(242, 196, 196);
+            button3.Location = new Point(1187, 91);
+            button3.Name = "button3";
+            button3.Size = new Size(149, 45);
+            button3.TabIndex = 5;
+            button3.Text = "Dar de baja";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(65, 206);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(1299, 568);
+            dataGridView1.TabIndex = 6;
+            // 
+            // frmGestionUsuarios
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1430, 848);
+            Controls.Add(dataGridView1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "frmGestionUsuarios";
+            Text = "Gestion de Usuarios";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private DataGridView dataGridView1;
+    }
+}
