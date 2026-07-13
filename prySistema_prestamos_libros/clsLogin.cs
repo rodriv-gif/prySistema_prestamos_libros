8b6e26dc851e89271705e057f176416be8523344
uriele-boop <20250997@uthh.edu.mx>
2026-07-13 17:54:09 -0600

    creo uriel inicie creando los atributos para poder comenzar a hacer que funcione nuestro login, y se realizo el primer metodo donde damos permisos exclusivos

--- prySistema_prestamos_libros/clsLogin.cs
+++ prySistema_prestamos_libros/clsLogin.cs
@@ -7,6 +7,34 @@ namespace prySistema_prestamos_libros
 {
     internal class clsLogin
     {
-        
+        private string usuario;
+        private string password;
+
+        public string Usuario { get => usuario; set => usuario = value; }
+        public string Password { get => password; set => password = value; }
+
+        //atributo estático
+        private static string perfil;
+        private static bool esAdministrador;
+        private static bool esBibliotecario;
+
+        //propiedades
+        public static bool EsAdministrador { get => esAdministrador; }
+        public static bool EsBibliotecario { get => esBibliotecario; }
+
+        public void AsignarPermisos()
+        {
+            switch (perfil)
+            {
+                case "Administrador":
+                    esAdministrador = true;
+                    esBibliotecario = false;
+                    break;
+                case "Bibliotecario":
+                    esAdministrador = false;
+                    esBibliotecario = true;
+                    break;
+            }
+        }
     }
 }
